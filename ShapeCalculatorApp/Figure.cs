3b9b21c980fb21e5bbcb542b5fb29c78e23aa9b3
Nadezhda <nadyachery@gmail.com>
2023-10-08 23:09:27 +0300

    ShapeCalculatorApp

---
+++ ShapeCalculatorApp/Figure.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculatorApp
+{
+    abstract class Figure
+    {
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+
+    class Circle : Figure
+    {
+        private int radius2;
+        private int radius2_read_count;
+        public int radius {
+            get { this.radius2_read_count++;
+                return this.radius2;
+            } private set { this.radius2 = value; } }
+
+        public Circle(int radius)
+        {
+            if (radius <= 0)
+                throw new ArgumentException("Значение радиуса круга должно быть положительным числом.");
+
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+
+    class Square : Figure
+    {
+        public int side { get; private set; }
+
+        public Square(int side)
+        {
+            if (side <= 0)
+                throw new ArgumentException("Значения сторон квадрата должны быть положительными числами.");
+
+            this.side = side;
+        }
+
+        public override double CalculateArea()
+        {
+            return side * side;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 4 * side;
+        }
+    }
+
+    class Rectangle : Figure
+    {
+        public int length { get; private set; }
+        public int width { get; private set; }
+
+        public Rectangle(int length, int width)
+        {
+            if (length <= 0 || width <= 0)
+                throw new ArgumentException("Значения сторон прямоугольника должны быть положительными числами.");
+
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (length + width);
+        }
+    }
+
+    class Triangle : Figure
+    {
+        public int a { get; private set; }
+        public int b { get; private set; }
+        public int c { get; private set; }
+
+        public Triangle(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new ArgumentException("Значения сторон треугольника должны быть положительными числами.");
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+                throw new ArgumentException("Сумма любых двух сторон треугольника должна быть больше третьей стороны.");
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override double CalculateArea()
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ ShapeCalculatorApp/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeCalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        private Figure selectedFigure;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            selectedFigure = null;
+        }
+
+        private void DrawCircle(Circle circle)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+
+            float x = (pictureBox1.Width - circle.radius) / 2;
+            float y = (pictureBox1.Height - circle.radius) / 2;
+
+            g.DrawEllipse(Pens.Black, x, y, circle.radius, circle.radius);
+        }
+
+        private void DrawSquare(Square square)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+
+            float x = (pictureBox1.Width - square.side) / 2;
+            float y = (pictureBox1.Height - square.side) / 2;
+
+            g.DrawRectangle(Pens.Black, x, y, square.side, square.side);
+        }
+
+        private void DrawRectangle(Rectangle rectangle)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+
+            float x = (pictureBox1.Width - rectangle.length) / 2;
+            float y = (pictureBox1.Height - rectangle.width) / 2;
+
+            g.DrawRectangle(Pens.Black, x, y, rectangle.length, rectangle.width);
+        }
+
+        private void DrawTriangle(Triangle triangle)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+
+            double x = (Math.Pow(triangle.a, 2) + Math.Pow(triangle.c, 2) - Math.Pow(triangle.b, 2)) / (2 * triangle.a);
+            double y = Math.Sqrt(Math.Pow(triangle.c, 2) - x * x);
+
+            Point point1 = new Point(125, 100);
+            Point point2 = new Point(125 + triangle.a, 100);
+            Point point3 = new Point(125 + (int)x, 100 + (int)y);
+
+            g.DrawLine(Pens.Black, point1, point2);
+            g.DrawLine(Pens.Black, point2, point3);
+            g.DrawLine(Pens.Black, point3, point1);
+        }
+
+        private void UpdateResults(Figure figure)
+        {
+            if (figure != null)
+            {
+                double area = figure.CalculateArea();
+                double perimeter = figure.CalculatePerimeter();
+                label1.Text = $"Площадь: {area:F2}, Периметр: {perimeter:F2}";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            selectedFigure = new Triangle(100, 100, 100);
+            DrawTriangle((Triangle)selectedFigure);
+            UpdateResults(selectedFigure);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            selectedFigure = new Rectangle(100, 60);
+            DrawRectangle((Rectangle)selectedFigure);
+            UpdateResults(selectedFigure);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            selectedFigure = new Square(80);
+            DrawSquare((Square)selectedFigure);
+            UpdateResults(selectedFigure);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selectedFigure = new Circle(100);
+            DrawCircle((Circle)selectedFigure);
+            UpdateResults(selectedFigure);
+        }
+    }
+}
